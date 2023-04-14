97df88e82592a87060dcb6602d90489ceb43aa1c
kkkovalov <vlad_kovalov@icloud.com>
2023-04-13 21:36:58 -0700

    Added descending type to insertion sort.

--- index.cpp
+++ index.cpp
@@ -2,23 +2,33 @@
 
 // Complexity - (n^2);
 
-void sortit(int *arr, int index, int n){
+void sortit(int *arr, int index, int n, int type){
     if(index == 0) return ;
     else if(index < n && index > 0) {
-        if(arr[index-1] > arr[index]) {
+        if(type){
+            if(arr[index-1] > arr[index]) {
             int temp = arr[index-1];
             arr[index - 1] = arr[index];
             arr[index] = temp;
-            sortit(arr, index-1, n);
-        } else return ;
+            sortit(arr, index-1, n, type);
+            } else return ;
+        } else {
+            if(arr[index-1] < arr[index]) {
+            int temp = arr[index-1];
+            arr[index - 1] = arr[index];
+            arr[index] = temp;
+            sortit(arr, index-1, n, type);
+            } else return ;
+        }
+        
     } else return ;
 };
 
 //By default sort is in ascending(increasing) order, to choose descending(decreasing) order set "type" parameter to 0;
 void insertionSort(int *arr, int n, int type = 1){
     int i = 0;
-    while( i < n){
-        sortit(arr, i, n);
+        while( i < n){
+        sortit(arr, i, n, type);
         i++;
     };
 };
@@ -28,7 +38,7 @@ void insertionSort(int *arr, int n, int type = 1){
 int main(void){
     const int n = 8;
     int array[8] = {5,3,1,2,9,4,7,6};
-    insertionSort(array, n);
+    insertionSort(array, n, 0);
     int i = 0;
     while(i < n){
         std::cout<<array[i]<<' ';
